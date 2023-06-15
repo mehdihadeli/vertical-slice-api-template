ec5877871c4a9c76ca123321cfab6f3c77305043
mehdihadeli <mehdi.hadeli@outlook.com>
2023-06-15 21:11:03 +0330

    refactor: :recycle: some refactoring

--- src/Vertical.Slice.Template/Shared/Extensions/WebApplicationBuilderExtensions/WebApplicationBuilderExtensions.ProblemDetails.cs
+++ src/Vertical.Slice.Template/Shared/Extensions/WebApplicationBuilderExtensions/WebApplicationBuilderExtensions.ProblemDetails.cs
@@ -14,6 +14,11 @@ public static partial class WebApplicationBuilderExtensions
             problemDetailsOptions.CustomizeProblemDetails = problemDetailContext =>
             {
                 // with help of capture exception middleware for capturing actual exception
+                // https://github.com/dotnet/aspnetcore/issues/4765
+                // https://github.com/dotnet/aspnetcore/pull/47760
+                // `problemDetailContext` doesn't contain real `exception` it will add in this pull request in .net 8
+                // .net 8 will add `IExceptionHandlerFeature`in `DisplayExceptionContent` and `SetExceptionHandlerFeatures` methods `DeveloperExceptionPageMiddlewareImpl` class, exact functionality of CaptureException
+                // bet before .net 8 preview 5 we should add `IExceptionHandlerFeature` manually with our `UseCaptureException`
                 if (problemDetailContext.HttpContext.Features.Get<IExceptionHandlerFeature>() is { } exceptionFeature)
                 { }
             };
--- src/Vertical.Slice.Template/Shared/Extensions/WebApplicationExtensions/WebApplicationExtensions.Infrastructure.cs
+++ src/Vertical.Slice.Template/Shared/Extensions/WebApplicationExtensions/WebApplicationExtensions.Infrastructure.cs
@@ -26,6 +26,8 @@ public static partial class WebApplicationExtensions
 
             // https://github.com/dotnet/aspnetcore/issues/4765
             // https://github.com/dotnet/aspnetcore/pull/47760
+            // .net 8 will add `IExceptionHandlerFeature`in `DisplayExceptionContent` and `SetExceptionHandlerFeatures` methods `DeveloperExceptionPageMiddlewareImpl` class, exact functionality of CaptureException
+            // bet before .net 8 preview 5 we should add `IExceptionHandlerFeature` manually with our `UseCaptureException`
             app.UseCaptureException();
         }
 
