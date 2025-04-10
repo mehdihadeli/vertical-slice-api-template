0527230e4cbb94cf622c02d21072de0321a32fd3
Mehdi Hadeli <mehdihadeli@gmail.com>
2025-04-10 17:47:30 +0200

    test: 🧪 add some unit-tests (#30)

--- tests/Vertical.Slice.Template.UnitTests/Products/Features/CreatingProduct/v1/CreateProductTests.cs
+++ tests/Vertical.Slice.Template.UnitTests/Products/Features/CreatingProduct/v1/CreateProductTests.cs
@@ -1,7 +1,7 @@
+using System.Diagnostics.CodeAnalysis;
 using AutoBogus;
 using FluentAssertions;
 using Mediator;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using NSubstitute;
 using NSubstitute.ExceptionExtensions;
@@ -14,6 +14,7 @@ using Vertical.Slice.Template.UnitTests.Common;
 namespace Vertical.Slice.Template.UnitTests.Products.Features.CreatingProduct.v1;
 
 [Collection(UnitTestCollection.Name)]
+[SuppressMessage("Reliability", "CA2012:Use ValueTasks correctly")]
 public class CreateProductTests : CatalogsUnitTestBase
 {
     [CategoryTrait(TestCategory.Unit)]
@@ -23,8 +24,11 @@ public class CreateProductTests : CatalogsUnitTestBase
         var createProductMock = new AutoFaker<CreateProduct>().Generate();
 
         var executor = Substitute.For<DbExecuters.CreateAndSaveProductExecutor>();
+
         executor
-            .Invoke(Arg.Is<Product>(x => x.Id == createProductMock.Id), Arg.Any<CancellationToken>())
+            .Invoke(
+                Arg.Is<Product>(x => x.Id == createProductMock.Id),
+                Arg.Any<CancellationToken>())
             .Returns(new ValueTask());
 
         // Arrange
@@ -32,18 +36,90 @@ public class CreateProductTests : CatalogsUnitTestBase
             executor,
             Substitute.For<IMediator>(),
             Substitute.For<IExternalEventBus>(),
-            NullLogger<CreateProductHandler>.Instance
-        );
+            NullLogger<CreateProductHandler>.Instance);
 
         // Act
-        var createdCustomerResponse = await handler.Handle(createProductMock, CancellationToken.None);
+        var createdCustomerResponse =
+            await handler.Handle(createProductMock, CancellationToken.None);
 
         //Assert
         await executor
             .Received(1)
-            .Invoke(Arg.Is<Product>(x => x.Id == createProductMock.Id), Arg.Any<CancellationToken>());
+            .Invoke(
+                Arg.Is<Product>(x => x.Id == createProductMock.Id),
+                Arg.Any<CancellationToken>());
+
         createdCustomerResponse.Should().NotBeNull();
         createdCustomerResponse.Id.Should().Be(createProductMock.Id);
+    }
+
+    [CategoryTrait(TestCategory.Unit)]
+    [Fact]
+    public async Task Handle_ShouldPublishEventsWithCorrectData_WhenProductIsCreated()
+    {
+        // Arrange
+        var createProductMock = new AutoFaker<CreateProduct>().Generate();
+
+        var mediatorMock = Substitute.For<IMediator>();
+        var eventBusMock = Substitute.For<IExternalEventBus>();
+        var executorMock = Substitute.For<DbExecuters.CreateAndSaveProductExecutor>();
+
+        mediatorMock.Publish(
+                Arg.Is<ProductCreatedDomainEvent>(
+                    e =>
+                        e.Id == createProductMock.Id &&
+                        e.Name == createProductMock.Name &&
+                        e.CategoryId == createProductMock.CategoryId &&
+                        e.Price == createProductMock.Price &&
+                        e.Description == createProductMock.Description),
+                Arg.Any<CancellationToken>())
+            .Returns(ValueTask.CompletedTask);
+
+
+        eventBusMock.PublishAsync(
+            Arg.Is<ProductCreatedIntegrationEventV1>(
+                e =>
+                    e.Id == createProductMock.Id &&
+                    e.Name == createProductMock.Name &&
+                    e.CategoryId == createProductMock.CategoryId &&
+                    e.Price == createProductMock.Price &&
+                    e.Description == createProductMock.Description),
+            null,
+            Arg.Any<CancellationToken>())
+            .Returns(Task.CompletedTask);
+
+        var handler = new CreateProductHandler(
+            executorMock,
+            mediatorMock,
+            eventBusMock,
+            NullLogger<CreateProductHandler>.Instance);
+
+        // Act
+        await handler.Handle(createProductMock, CancellationToken.None);
+
+        // Assert
+        await mediatorMock.Received(1)
+            .Publish(
+                Arg.Is<ProductCreatedDomainEvent>(
+                    e =>
+                        e.Id == createProductMock.Id &&
+                        e.Name == createProductMock.Name &&
+                        e.CategoryId == createProductMock.CategoryId &&
+                        e.Price == createProductMock.Price &&
+                        e.Description == createProductMock.Description),
+                Arg.Any<CancellationToken>());
+
+        await eventBusMock.Received(1)
+            .PublishAsync(
+                Arg.Is<ProductCreatedIntegrationEventV1>(
+                    e =>
+                        e.Id == createProductMock.Id &&
+                        e.Name == createProductMock.Name &&
+                        e.CategoryId == createProductMock.CategoryId &&
+                        e.Price == createProductMock.Price &&
+                        e.Description == createProductMock.Description),
+                null,
+                Arg.Any<CancellationToken>());
     }
 
     [CategoryTrait(TestCategory.Unit)]
@@ -53,13 +129,19 @@ public class CreateProductTests : CatalogsUnitTestBase
         var validator = new CreateProductValidator();
 
         var invalidProducts = new[]
-        {
-            new CreateProduct("", Guid.NewGuid(), 10), // Empty name
-            new CreateProduct("Pr", Guid.NewGuid(), 10), // Name too short
-            new CreateProduct("ValidName", Guid.Empty, 10), // Empty CategoryId
-            new CreateProduct("ValidName", Guid.NewGuid(), -1), // Negative price
-            new CreateProduct("ValidName", Guid.NewGuid(), 0), // Zero price
-        };
+                              {
+                                  new CreateProduct("", Guid.NewGuid(), 10), // Empty name
+                                  new CreateProduct("Pr", Guid.NewGuid(), 10), // Name too short
+                                  new CreateProduct(
+                                      "ValidName",
+                                      Guid.Empty,
+                                      10), // Empty CategoryId
+                                  new CreateProduct(
+                                      "ValidName",
+                                      Guid.NewGuid(),
+                                      -1), // Negative price
+                                  new CreateProduct("ValidName", Guid.NewGuid(), 0), // Zero price
+                              };
 
         foreach (var invalidProduct in invalidProducts)
         {
@@ -75,16 +157,18 @@ public class CreateProductTests : CatalogsUnitTestBase
         var createProductMock = new AutoFaker<CreateProduct>().Generate();
 
         var executor = Substitute.For<DbExecuters.CreateAndSaveProductExecutor>();
-        executor.Invoke(Arg.Any<Product>(), Arg.Any<CancellationToken>()).Throws(new Exception("Database error"));
+
+        executor.Invoke(Arg.Any<Product>(), Arg.Any<CancellationToken>())
+            .Throws(new Exception("Database error"));
 
         var handler = new CreateProductHandler(
             executor,
             Substitute.For<IMediator>(),
             Substitute.For<IExternalEventBus>(),
-            NullLogger<CreateProductHandler>.Instance
-        );
+            NullLogger<CreateProductHandler>.Instance);
 
-        Func<Task> act = async () => await handler.Handle(createProductMock, CancellationToken.None);
+        Func<Task> act = async () =>
+                             await handler.Handle(createProductMock, CancellationToken.None);
 
         await act.Should().ThrowAsync<Exception>().WithMessage("Database error");
     }
